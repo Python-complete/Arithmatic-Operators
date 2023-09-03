0e2a2b4841c56f2c74030cf24533f2136a96ab4d
Arpan Basu <arpanbasu2002@gmail.com>
2023-09-03 21:50:23 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,16 @@
+n = int(input("Enter a number\n"))
+n1 = int(input("Enter the other number\n"))
+sum = n + n1
+print("Sum = " , sum)
+sub = n - n1
+print("Subtraction = " , sub)
+mul = n * n1
+print("Multipication = " , mul)
+div = n / n1                        # Float 
+print("Divison = " , div) 
+div1 = n // n1                      #Integer
+print("Division = " ,div1)
+per = n % n1
+print("percent = " , per)
+power = n ** n1
+print("To the power ", power)
